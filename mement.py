c0b32d5df70c7daadd3a201189f48abb6f3aabd6
trsqxyz <trsqxyz@gmail.com>
2015-04-04 17:28:41 +0900

    Create mori function as a wait for 5 min and return start Epoch time.

---
+++ mement.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import time
+
+class Mement(object):
+    """docstring for Mement"""
+    def __init__(self, arg):
+        pass
+    def mori(self):
+        start_time = time.time()
+        time.sleep(60*5)
+        return start_time 
