f2bd9f6cb59ca3c0333727973a97e3b815387544
trsqxyz <trsqxyz@gmail.com>
2015-04-04 23:45:48 +0900

    Create first mement mori mode.

--- mement.py
+++ mement.py
@@ -2,12 +2,29 @@
 # -*- coding: utf-8 -*-
 
 import time
+import threading
 
-class Mement(object):
-    """docstring for Mement"""
-    def __init__(self, arg):
-        pass
-    def mori(self):
+class Mori(object):
+    """docstring for Mori"""
+    def __init__(self, mode=None, sec=60*5):
+        self.mode = {
+            'mori': self.mori,
+            None: self.mori
+            }
+        self.stop_event = threading.Event()
+        self.t = threading.Timer(1, self.mode[mode], (sec,))
+        self.t.start()
+
+    def mori(self, sec):
         start_time = time.time()
-        time.sleep(60*5)
-        return start_time 
+        while not self.stop_event.is_set():
+            print("Mement mori.")
+            if sec < time.time() - start_time:
+                self.stop()
+            time.sleep(sec)
+
+    def stop(self):
+        self.stop_event.set()
+
+if __name__ == '__main__':
+    m = Mori('mori')
